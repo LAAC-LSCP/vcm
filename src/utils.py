ac933fbe146d0edc3aed4531afc79ce17beaf1e7
William N. Havard <william.havard@gmail.com>
2022-01-10 19:18:00 +0100

    Added utility function to recursively list files + removed sox-based _seg_audio function

--- src/utils.py
+++ src/utils.py
@@ -2,14 +2,21 @@
 # -*- coding: utf8 -*-
 
 import os
+import tqdm
 from subprocess import PIPE, run
 import librosa
 import soundfile
+
+#
+# General utility functions
+#
 
 def _clean(feature_output_path):
     if os.path.isfile(feature_output_path):
         os.remove(feature_output_path)
 
+def get_raw_filename(path):
+    return os.path.splitext(os.path.basename(path))[0]
 
 def read_text_file(input_path):
     with open(input_path) as file_in:
@@ -21,6 +28,22 @@ def dump_text_file(output_path, lines):
         file_out.write('\n'.join(lines))
 
 
+def find_all_files(path, extension=''):
+    audiofile_name2path = {}
+
+    for p, d, f in os.walk(path):
+        for file in f:
+            if extension != '':
+                if file.endswith(extension):
+                    audiofile_name2path[get_raw_filename(file)] = os.path.join(p, file)
+            else:
+                audiofile_name2path[get_raw_filename(file)] = os.path.join(p, file)
+    return audiofile_name2path
+
+#
+# VCM utility functions
+#
+
 def extract_feature(audio_input_path, feature_output_path, SMILEXTRACT_PATH):
     config = os.path.join(os.path.dirname(__file__), '../config/gemaps/eGeMAPSv01a.conf')
 
@@ -30,15 +53,6 @@ def extract_feature(audio_input_path, feature_output_path, SMILEXTRACT_PATH):
     result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
 
     return result.returncode, result.stdout, result.stderr
-
-
-def _seg_audio(input_audio, output_audio, onset, duration):
-    # Original code
-    assert os.path.isfile(input_audio)
-
-    cmd_seg = "sox " + input_audio + " " + output_audio + " trim " + " " + onset + " " + duration
-    subprocess.call(cmd_seg, shell=True)
-
 
 def seg_audio(input_audio, output_audio, onset, duration):
     # onset and duration should be values provided in SECONDS

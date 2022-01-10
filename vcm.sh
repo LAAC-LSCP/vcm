b75e73d62407d690a84e2e11a424eb79a553cf85
William N. Havard <william.havard@gmail.com>
2022-01-10 10:24:38 +0100

    Added bash script

---
+++ vcm.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+usage() {
+    # Lazy call to Python's help message
+    python $RUN_PATH --help | sed 's/vcm.py/vcm.sh/g'
+    exit 1;
+}
+INPUT_AUDIO_PATH=''
+INPUT_RTTM_PATH=''
+SMILEXTRACT_BIN_PATH=''
+OUTPUT_VCM_PATH=''
+ALL_CHILDREN=''
+KEEP_OTHER=''
+KEEP_TEMP=''
+REUSE_TEMP=''
+
+RUN_PATH='./src/vcm.py'
+
+# Arguments parser
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -a|--input-audio-path)
+    INPUT_AUDIO_PATH=$2
+    shift
+    shift
+    ;;
+    -r|--input-rttm-path)
+    INPUT_RTTM_PATH=$2
+    shift
+    shift
+    ;;
+    -s|--smilextract-bin-path)
+    SMILEXTRACT_BIN_PATH=$2
+    shift
+    shift
+    ;;
+    -o|--output-vcm-path)
+    OUTPUT_VCM_PATH='--output-vcm-path '$2
+    shift
+    shift
+    ;;
+    --all-children)
+    ALL_CHILDREN='--all-children'
+    shift # past argument
+    ;;
+    --keep-other)
+    KEEP_OTHER='--keep-other'
+    shift # past argument
+    ;;
+    --keep-temp)
+    KEEP_TEMP='--keep-temp'
+    shift # past argument
+    ;;
+    --reuse-temp)
+    REUSE_TEMP='--reuse-temp'
+    shift # past argument
+    ;;
+    -h|--help)
+    shift
+    usage
+    ;;
+    *)
+    echo "/!\ Unknown option '$1'. Not running anything" # Unknown option
+    usage
+    ;;
+  esac
+done
+
+if [[ -z $INPUT_AUDIO_PATH ]] | [[ -z $INPUT_RTTM_PATH ]] | [[ -z $SMILEXTRACT_BIN_PATH ]]; then
+  usage
+fi
+
+echo $RUN_PATH --input-audio-path $INPUT_AUDIO_PATH --input-rttm-path $INPUT_RTTM_PATH --smilextract-bin-path $SMILEXTRACT_BIN_PATH \
+                 $OUTPUT_VCM_PATH $ALL_CHILDREN $KEEP_OTHER $KEEP_TEMP $REUSE_TEMP
+
+python $RUN_PATH --input-audio-path $INPUT_AUDIO_PATH --input-rttm-path $INPUT_RTTM_PATH --smilextract-bin-path $SMILEXTRACT_BIN_PATH \
+                 $OUTPUT_VCM_PATH $ALL_CHILDREN $KEEP_OTHER $KEEP_TEMP $REUSE_TEMP
